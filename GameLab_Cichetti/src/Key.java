36d53d4f3f516b8ffd69ae4e53ae5af2ee11823b
cichetti <cichetti@sputnik.cs.widener.edu>
2024-11-07 15:13:01 -0500

    new class key

--- GameLab_Cichetti/src/Key.java
+++ GameLab_Cichetti/src/Key.java
@@ -1,4 +1,22 @@
 
-public class Key {
+public class Key extends Item {
+	
+	public Key(String name3, String description3) {
+		super(name3, description3);
+	}
+	public void use() {
+		Room exit = Game.currentRoom.getExit('s');
+		if(exit != null && exit.getName().equals("Exit")){
+			exit.setLock(false);
+			Game.print("Door has been unlocked");
+		}else {
+			Game.print("Door cannot be locked");
+		
+		
+		}
+		
+			
+	}
+
 
 }
